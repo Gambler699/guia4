6ac611af297dc51f4bc58ecaa56c17d25cfed912
rodrigo <020200394j@uandina.edu.pe>
2025-08-31 21:41:58 -0500

    Primer commit - proyectos guia4

---
+++ ejercicio1y2/android/app/src/main/kotlin/com/example/ejercicio1y2/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.ejercicio1y2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ ejercicio3y4/android/app/src/main/kotlin/com/example/ejercicio3y4/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.ejercicio3y4
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
